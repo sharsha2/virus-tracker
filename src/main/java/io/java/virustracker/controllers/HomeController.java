4a8c49f02b833d47fece7259d6c9a84167cfd7f6
sharsha2 <karuturiharsh@gmail.com>
2021-06-14 16:27:08 -0400

    Initial commit

---
+++ src/main/java/io/java/virustracker/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package io.java.virustracker.controllers;
+
+import io.java.virustracker.models.LocationStats;
+import io.java.virustracker.services.VirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    VirusDataService virusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = virusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+}
---
+++ src/test/java/io/java/virustracker/VirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.virustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
